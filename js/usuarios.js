677f01325f8604f4dca6468e51afb217788526b0
Pan-batido <escarleth.canon@gmail.com>
2026-09-04 20:55:17 -0400

    correccion en admin

---
+++ js/usuarios.js
@@ -0,0 +1,14 @@
+const usuariosBase = [
+  {
+    rut: "12345678-9",
+    nombre: "Sakura De Naruto",
+    correo: "admin@gmail.com",
+    rol: "Administrador"
+  },
+  {
+    rut: "98765432-1",
+    nombre: "Juan Pérez",
+    correo: "juan.perez@gmail.com",
+    rol: "Cliente"
+  }
+];
